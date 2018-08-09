6b6c8f960c4227354b0442d8396d92849534bbc1
chris.piatt@publicisoneteam.com <chris@pyython.com>
2018-08-09 11:18:54 -0400

    style color fix

--- usability/annotations/main.js
+++ usability/annotations/main.js
@@ -113,7 +113,8 @@ define([
         var date = format_date_to_today_moment(comment.date);
         var $comment_field = $('<div/>', {class: 'comment_field'});
         var commenter_number = IPython.notebook.metadata["commenters"].indexOf(comment.username.toLowerCase().trim());
-        var $username = $('<span/>', {class: 'comment_username', html: comment.username + ":", color: colors[commenter_number]});
+        console.log(commenter_number);
+        var $username = $('<span/>', {class: 'comment_username', html: comment.username + ":", style: "color: " + colors[commenter_number]});
         var $date = $('<span/>', {class: 'comment_date', html: date});
         var $heading = $('<h4/>', {class: 'comment_heading'});
         var $comment = $('<p/>', {html: insert_line_breaks(comment.comment), class: "comment_text"});
